591f58e6abb33b673215edc9abc987264f50c1d6
Gavin Cooper <Gavin.Cooper@newcastle.edu.au>
2016-04-29 14:19:56 +1000

    Break up code into functions

--- checkup.py
+++ checkup.py
@@ -1,24 +1,33 @@
 # coding: utf-8
-import sys, os
+import os
 import matplotlib.pyplot as plt
+import pandas as pd
 from prespy.logfile import load
 
+
 logfiles = []
-for path in os.listdir('.'):
-    if os.path.isfile(path) and path.endswith('.log'):
-        logfiles.append(load(path))
-        
-resp_classes = {'hit':1, 'miss':2, 'incorrect':3}
-lengths = {'LONG':1, 'SHORT':2}
-deviant = {'EHD':1, 'LD':2, 'other':3}
+resp_classes = {'hit': 1, 'miss': 2, 'incorrect': 3}
+lengths = {'LONG': 1, 'SHORT': 2}
+deviant = {'EHD': 1, 'LD': 2, 'other': 3}
 
-for l in logfiles:
-    soundevts = [e for e in l.events if e.etype == 'Sound']
-    responses = [resp_classes[e.data['Stim Type']] for e in soundevts]
-    plt.scatter(list(range(len(responses))), responses)
-    plt.yticks(sorted(resp_classes.values()), 
-               sorted(resp_classes, key=resp_classes.get))
-    plt.title(l.subjectID)
-    plt.show()
-    
 
+def loadem():
+    for path in os.listdir('.'):
+        if os.path.isfile(path) and path.endswith('.log'):
+            logfiles.append(load(path))
+
+
+def scatterem():
+    for l in logfiles:
+        soundevts = [e for e in l.events if e.etype == 'Sound']
+        responses = [resp_classes[e.data['Stim Type']] for e in soundevts]
+        plt.scatter(list(range(len(responses))), responses)
+        plt.yticks(sorted(resp_classes.values()),
+                   sorted(resp_classes, key=resp_classes.get))
+        plt.title(l.subjectID)
+        plt.show()
+
+def classifyem():
+    for l in logfiles:
+        df = pd.DataFrame([e.data for e in l.events])
+        df
