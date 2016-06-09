364987d2791879aa552e98f01d75d21e2ccd145c
Gavin Cooper <Gavin.Cooper@newcastle.edu.au>
2016-06-09 12:15:07 +1000

    Refresh, clean and generalise code

--- view.py
+++ view.py
@@ -1,61 +1,53 @@
 from prespy.logfile import load
+from eegevt.eventfile import load_efile
 import matplotlib.pyplot as plt
 import sys
 from itertools import zip_longest
 import json
 
 
-def plot_codes(pfile, plot=True):
-    codes = [e.code for e in pfile.events if e.etype == 'Sound']
-    if not plot:
-        return codes
-    plt.plot(codes)
-    plt.show()
-    return codes
-
-def plot_diffs(pfile, plot=True):
-    times = [e.time for e in pfile.events if e.etype == 'Sound']
-    diffs = [t - times[i] for i, t in enumerate(times[1:])]
-    if not plot:
-        return diffs
-    plt.plot(diffs)
-    plt.show()
-    return diffs
-
-def extract_ev2(efile):
-    pcodes = [e[1] for e in efile if e[1] not in {'254', '251', '255'}]
-    return pcodes
-
 def compare_codes(codes, pcodes):
     mismatches = 0
-    for i, c1, c2 in zip_longest(range(max(len(codes), len(pcodes))), codes, pcodes):
+    for i, c1, c2 in zip_longest(range(max(len(codes), len(pcodes))),
+                                 codes, pcodes):
         if c1 != c2:
-            print(i,c1,c2)
+            print(i, c1, c2)
             mismatches += 1
         if mismatches > 20:
             print(len(codes), len(pcodes))
             break
 
-def main_alt():
-    fn = sys.argv[1]
-    pfile = load(fn)
-    with open(sys.argv[2], 'r') as efcnts:
-        lines = efcnts.read().splitlines()
-    efile = [l.split() for l in lines]
-    codes = plot_codes(pfile, False)
-    plot_diffs(pfile, False)
-    pcodes = extract_ev2(efile)
-    compare_codes(codes, pcodes)
 
-def main(lf, ef):
+def load_pair(lf, ef):
     pfile = load(lf)
-    with open(ef, 'r') as efcnts:
-        lines = efcnts.read().splitlines()
-    efile = [l.split() for l in lines]
-    codes = plot_codes(pfile)
-    plot_diffs(pfile)
-    pcodes = extract_ev2(efile)
-    compare_codes(codes, pcodes)
+    efile = load_efile(ef)
+    # load codes
+    pcodes = [e.code for e in pfile.events]
+    ecodes = [d[efile.evtcode] for d in efile.data]
+    # load time differences between events
+    ptimes = [e.time for e in pfile.events]
+    etimes = [d[efile.evttime] for d in efile.data]
+    pdiffs = [t - ptimes[i] for i, t in enumerate(ptimes[1:])]
+    ediffs = [(t - etimes[i])*1000 for i, t in enumerate(etimes[1:])]
+    # Return report
+    return dict(codes=(pcodes, ecodes), diffs=(pdiffs, ediffs))
+
+
+def display(report):
+    plt.figure(1)
+    plt.hold = True
+    ax = plt.subplot(211)
+    c = report['codes']
+    ax.plot(c[0], 'b--')
+    ax.plot(c[1], 'r--')
+    ax.set_title('Codes vs Event Number')
+    ax = plt.subplot(212)
+    d = report['diffs']
+    ax.plot(d[0], 'b--')
+    ax.plot(d[1], 'r--')
+    ax.set_title('SOA vs Event Number')
+    plt.show()
+
 
 def runfromfile(jsfile):
     with open(jsfile, 'r') as jsonfile:
@@ -64,7 +56,11 @@ def runfromfile(jsfile):
     for key in dirdict:
         print(key)
         for filepair in dirdict[key]:
-            main(*filepair)
+            print('File Pair: ', filepair)
+            if input('Run? (Y/N)') == 'Y':
+                report = load_pair(*filepair)
+                display(report)
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2])
+    report = load_pair(sys.argv[1], sys.argv[2])
+    display(report)
